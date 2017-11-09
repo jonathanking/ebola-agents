8a70810e2d424bc5affd2413cbadc18697562550
jonathanking <jonathanking@berkeley.edu>
2017-11-09 11:26:41 -0500

    added global set objects for faster membership testing; i.e. maintaining a set of dictionary keys instead of calling dictionary.keys(). Formatting

--- Agent.py
+++ Agent.py
@@ -10,55 +10,58 @@ if len(sys.argv) < 3:
 MAX_TIME, REDUCTION_FACTOR = int(sys.argv[1]), int(sys.argv[2])
 
 # Data Lookup Structures
-NEIGHBOR_LIST = {}        # (x,y) -> neighbors of (x,y)
-NODE_STATS = {}           # (x,y) -> {"numI": # infected, "nonR": # non_removed}
+NEIGHBOR_LIST = {}  # (x,y) -> neighbors of (x,y)
+NEIGHBOR_LIST_KEYS = set()
+NODE_STATS = {}  # (x,y) -> {"numI": # infected, "nonR": # non_removed}
 
 # Global Model Params
 OUT_DIR = "results/"
-GRAPH_SIZE = int(np.sqrt(174 - 1)/np.sqrt(REDUCTION_FACTOR)) # sqrt(square area of city) / sqrt(scaling_factor)
-NUM_AGENTS = int((1.6 * 10**6)/(REDUCTION_FACTOR)) # population / scaling factor
-NUM_NODES = (GRAPH_SIZE + 1)**2
+GRAPH_SIZE = int(np.sqrt(174 - 1) / np.sqrt(REDUCTION_FACTOR))  # sqrt(square area of city) / sqrt(scaling_factor)
+NUM_AGENTS = int((1.6 * 10 ** 6) / (REDUCTION_FACTOR))  # population / scaling factor
+NUM_NODES = (GRAPH_SIZE + 1) ** 2
 CUR_TIME = 0
 NUM_INF = 1
 NUM_R = 0
 NEW_CASES = 1
 
 # Agent Params
-CONSTRAIN_MOVEMENT = False
+CONSTRAIN_MOVEMENT = True
 CONSTRAIN_NETWORK = True
+
 if not CONSTRAIN_NETWORK:
     EXCLUDE_EDGES = dict()
 elif CONSTRAIN_NETWORK:
-    # (x,y) -> [(x0,y0), (x1,y1)...]; Neighbors to ignore
-    EXCLUDE_EDGES = {(0,6):(0,7),   \
-                     (1,6):(1,7),   \
-                     (2,6):(2,7),   \
-                     (3,6):(3,7),   \
-                     (4,6):(4,7),   \
-                     (5,6):(5,7),   \
-                     (6,0):(7,0),   \
-                     (6,1):(7,1),   \
-                     (6,2):(7,2),   \
-                     (6,3):(7,3),   \
-                     (6,4):(7,4),   \
-                     (6,5):(7,5),   \
-                     (6,8):(7,8),   \
-                     (6,9):(7,9),   \
-                     (6,10):(7,10), \
-                     (6,11):(7,11), \
-                     (6,12):(7,12), \
-                     (6,13):(7,13), \
-                     (8,6):(8,7),   \
-                     (9,6):(9,7),   \
-                     (10,6):(10,7), \
-                     (11,6):(11,7), \
-                     (12,6):(12,7), \
-                     (13,6):(13,7)}
+    # (x,y) -> [Neighbors to ignore]
+    EXCLUDE_EDGES = {(0, 6): (0, 7),
+                     (1, 6): (1, 7),
+                     (2, 6): (2, 7),
+                     (3, 6): (3, 7),
+                     (4, 6): (4, 7),
+                     (5, 6): (5, 7),
+                     (6, 0): (7, 0),
+                     (6, 1): (7, 1),
+                     (6, 2): (7, 2),
+                     (6, 3): (7, 3),
+                     (6, 4): (7, 4),
+                     (6, 5): (7, 5),
+                     (6, 8): (7, 8),
+                     (6, 9): (7, 9),
+                     (6, 10): (7, 10),
+                     (6, 11): (7, 11),
+                     (6, 12): (7, 12),
+                     (6, 13): (7, 13),
+                     (8, 6): (8, 7),
+                     (9, 6): (9, 7),
+                     (10, 6): (10, 7),
+                     (11, 6): (11, 7),
+                     (12, 6): (12, 7),
+                     (13, 6): (13, 7)}
+EXCLUDE_EDGES_KEYS = set(EXCLUDE_EDGES.keys())
 PROB_STAY = 0.9
 PROB_LEAVE = 1 - PROB_STAY
-INF_TIME = 8.0             # From CDC avg time til death/removal
-R_VALUE = 2.0              # From CDC avg # of people infected by individual
-R0 = R_VALUE / INF_TIME    # Probability of infecting someone per timestep
+INF_TIME = 8.0  # From CDC avg time til death/removal
+R_VALUE = 2.0  # From CDC avg # of people infected by individual
+R0 = R_VALUE / INF_TIME  # Probability of infecting someone per timestep
 
 print "Constrain movement:", CONSTRAIN_MOVEMENT
 print "Constrain network:", CONSTRAIN_NETWORK
@@ -72,7 +75,7 @@ class Agent(object):
         self.state = state
         self.exposed_time = None
         self.infected_time = None
-        self.INC_TIME = np.random.randint(2, 21 + 1) # From WHO incubation time
+        self.INC_TIME = np.random.randint(2, 21 + 1)  # From WHO incubation time
         self.neighbors = self.find_neighbors()
 
         # Update stats
@@ -90,16 +93,15 @@ class Agent(object):
             NODE_STATS[self.pos]["nonR"] -= 1
 
         # Move to a neighboring position
+        num_neighbors = len(self.neighbors) - 1
         if CONSTRAIN_MOVEMENT and self.state == "I":
-            num_neighbors = len(self.neighbors) - 1
             probability_dist = [PROB_STAY]
-            for i in range(num_neighbors):
+            for i in xrange(num_neighbors):
                 probability_dist.append(PROB_LEAVE / num_neighbors)
-            idx = np.random.choice(range(len(self.neighbors)), p=probability_dist)
+            idx = np.random.choice(xrange(len(self.neighbors)), p=probability_dist)
             self.pos = self.neighbors[idx]
         else:
-            idx = np.random.choice(range(len(self.neighbors)))
-            self.pos = self.neighbors[idx]
+            self.pos = self.neighbors[np.random.randint(num_neighbors + 1)]
 
         # Add self to stats
         if self.state == "I":
@@ -133,7 +135,8 @@ class Agent(object):
 
     def find_neighbors(self):
         global NEIGHBOR_LIST
-        if self.pos in NEIGHBOR_LIST.keys():
+        global NEIGHBOR_LIST_KEYS
+        if self.pos in NEIGHBOR_LIST_KEYS:
             return NEIGHBOR_LIST[self.pos]
         else:
             n = [self.pos]
@@ -146,10 +149,11 @@ class Agent(object):
                         xy = (self.pos[0], self.pos[1] + d)
                     x1, y1 = xy
                     # True if this neighbor should be excluded
-                    exclude_neighbor = (self.pos in EXCLUDE_EDGES.keys()) and ((x1, y1) in EXCLUDE_EDGES[self.pos])
+                    exclude_neighbor = (self.pos in EXCLUDE_EDGES_KEYS) and ((x1, y1) in EXCLUDE_EDGES[self.pos])
                     if not (exclude_neighbor or x1 < 0 or y1 < 0 or x1 > GRAPH_SIZE or y1 > GRAPH_SIZE):
                         n.append((x1, y1))
             NEIGHBOR_LIST[self.pos] = n
+            NEIGHBOR_LIST_KEYS.add(self.pos)
             return n
 
     def __repr__(self):
@@ -164,8 +168,8 @@ def init_agents():
     print "agents_per_node", agents_per_node
     print "left_over_agents", left_over_agents
 
-    for x in range(GRAPH_SIZE + 1):
-        for y in range(GRAPH_SIZE + 1):
+    for x in xrange(GRAPH_SIZE + 1):
+        for y in xrange(GRAPH_SIZE + 1):
 
             if (x, y) not in NODE_STATS.keys():
                 NODE_STATS[(x, y)] = {"numI": 0.0, "nonR": 0.0}
@@ -173,7 +177,7 @@ def init_agents():
                 agents.append(Agent((x, y), "S"))
                 left_over_agents -= 1
 
-            for a in range(agents_per_node):
+            for a in xrange(agents_per_node):
                 state = "S"
                 if x == 0 and y == 0 and a == 0:
                     state = "I"
@@ -214,8 +218,8 @@ def main():
 
     np.save(get_filename(start_time, "data/INFECTED_") + ".npy", infected)
     np.save(get_filename(start_time, "data/INFCONLY_") + ".npy", infected_only)
-    plt.plot(range(MAX_TIME), infected, color='b', label='Infected + Removed')
-    plt.plot(range(MAX_TIME), infected_only, color='r', label='Infected')
+    plt.plot(xrange(MAX_TIME), infected, color='b', label='Infected + Removed')
+    plt.plot(xrange(MAX_TIME), infected_only, color='r', label='Infected')
     plt.title("CONSTRAIN_MOVEMENT = " + str(CONSTRAIN_MOVEMENT))
     plt.ylabel('Number of cases')
     plt.xlabel('Day #')
@@ -224,7 +228,7 @@ def main():
     # plt.show()
     plt.close()
 
-    plt.plot(range(len(new_cases)), new_cases, label='New cases')
+    plt.plot(xrange(len(new_cases)), new_cases, label='New cases')
     plt.title("New cases, CONSTRAIN_MOVEMENT =" + str(CONSTRAIN_MOVEMENT))
     plt.ylabel('Number of cases')
     plt.xlabel('Week #')
@@ -248,10 +252,9 @@ def get_filename(timestr, prefix=""):
         const_net = "True_"
     else:
         const_net = "False"
-    return OUT_DIR + "/" + prefix + "size{0:03d}_agents{1}_cm{2}_cn{3}_days{4}_t{5}".format(GRAPH_SIZE, NUM_AGENTS, const_mov, const_net, MAX_TIME, timestr)
-
-
-
+    return OUT_DIR + "/" + prefix + "size{0:03d}_agents{1}_cm{2}_cn{3}_days{4}_t{5}".format(GRAPH_SIZE, NUM_AGENTS,
+                                                                                            const_mov, const_net,
+                                                                                            MAX_TIME, timestr)
 
 
 if __name__ == '__main__':
